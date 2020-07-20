5166962eb6e725126533b4afb577f238c76dd484
jeffery graham <jeffery.lemar.graham@gmail.com>
2020-07-19 21:35:14 -0400

    chore: add logging middleware

--- index.js
+++ index.js
@@ -74,11 +74,39 @@ function goals(state = [], action) {
   }
 }
 
+const checker = (store) => (next) => (action) => {
+  if (
+    action.type === ADD_TODO &&
+    action.todo.name.toLowerCase().includes("bitcoin")
+  ) {
+    return alert("Nope. That's a bad idea.");
+  }
+
+  if (
+    action.type === ADD_GOAL &&
+    action.goal.name.toLowerCase().includes("bitcoin")
+  ) {
+    return alert("Nope. That's a bad idea.");
+  }
+
+  return next(action);
+};
+
+const logger = (store) => (next) => (action) => {
+  console.group(action.type);
+  console.log("The action ", action);
+  const result = next(action);
+  console.log("The new state is ", store.getState());
+  console.groupEnd();
+  return result;
+};
+
 const store = Redux.createStore(
   Redux.combineReducers({
     todos,
     goals,
-  })
+  }),
+  Redux.applyMiddleware(checker, logger)
 );
 
 store.subscribe(() => {
@@ -112,11 +140,6 @@ function addGoal() {
   input.value = "";
 
   store.dispatch(addGoalAction({ name, id: generateId() }));
-}
-
-function toggleTodo() {
-  const id = document.getElementById();
-  store.dispatch();
 }
 
 document.getElementById("todoBtn").addEventListener("click", addTodo);
